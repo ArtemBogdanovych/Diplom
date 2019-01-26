8f7becac156802aaa2c5baf8fd7fcc51a34897f3
Artem <Bogdanovychartem@gmail.com>
2019-01-26 16:23:30 +0200

    12

--- 12/src/main/java/DAO/TourDAO.java
+++
@@ -1,9 +0,0 @@
-package DAO;
-
-import java.util.List;
-
-public interface TourDAO {
-	TourData getTourbyID(int ID);
-
-	List<TourData> gettours();
-}
--- 12/src/main/java/DAO/UserDAO.java
+++
@@ -1,9 +0,0 @@
-package DAO;
-
-import java.util.List;
-
-public interface UserDAO {
-	UserData getUserbyID(int iduser);
-
-	List<UserData> getusers();
-}
